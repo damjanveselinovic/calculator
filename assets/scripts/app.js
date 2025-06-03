90cb0c1e97797bd5c93756d841b6b8ffd728074c
damjanveselinovic <veselinovic,damjan1@gmail.com>
2025-06-03 23:45:02 +0200

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,74 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function logObject(operator, resultBeforeCalc, calcNumber) {
+  const logEntry = {
+    operation: operator,
+    prevResult: resultBeforeCalc,
+    number: calcNumber,
+    newResult: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntry.operation);
+  console.log(logEntries);
+}
+
+function add() {
+  const enteredNumber = parseInt(userInput.value); //da ne bismo morali da menjamo svuda ako se userInput naziv promeni u vendoru, sad je vise flexable
+  const initialResult = currentResult;
+  //calcDesc = `${currentResult} + ${enteredNumber}`;
+  currentResult += enteredNumber;
+  //outputResult(currentResult, calcDesc);
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  /*const logEntry = {
+    operation: "ADD",
+    prevResult: initialResult,
+    number: enteredNumber,
+    newResult: currentResult,
+  };
+  //logEntries.push(enteredNumber);         //broj
+  logEntries.push(logEntry); //objekat
+  console.log(logEntry.operation);
+  console.log(logEntries);*/
+  logObject("ADD", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  logObject("SUBTRACT", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  logObject("MULTIPLY", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  logObject("DIVIDE", initialResult, enteredNumber);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+
+//currentResult = (currentResult + 10) * 3;
+
+//let calculationDescription = "(" + defaultResult + " + 10) * 3";
+//let calculationDescription2 = `(${defaultResult} + 10) * 3`;
