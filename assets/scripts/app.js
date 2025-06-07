293b92b50e0a286255553c5ea90ba7f98f00bc59
damjanveselinovic <veselinovic,damjan1@gmail.com>
2025-06-07 16:40:59 +0200

    simplified with an if statement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -19,6 +19,27 @@ function logObject(operator, resultBeforeCalc, calcNumber) {
   console.log(logEntries);
 }
 
+function calculateResult(operator) {
+  const enteredNumber = parseInt(userInput.value);
+  const initialResult = currentResult;
+  let mathSign;
+  if (operator == "ADD") {
+    currentResult += enteredNumber;
+    mathSign = "+";
+  } else if (operator == "SUBTRACT") {
+    currentResult -= enteredNumber;
+    mathSign = "-";
+  } else if(operator=="MULTIPLY"){
+    currentResult *= enteredNumber;
+    mathSign = "*";
+  } else{
+    currentResult /= enteredNumber;
+    mathSign = "/";
+  }
+  createAndWriteOutput(mathSign, initialResult, enteredNumber);
+  logObject(operator, initialResult, enteredNumber);
+}
+/*
 function add() {
   const enteredNumber = parseInt(userInput.value); //da ne bismo morali da menjamo svuda ako se userInput naziv promeni u vendoru, sad je vise flexable
   const initialResult = currentResult;
@@ -26,16 +47,16 @@ function add() {
   currentResult += enteredNumber;
   //outputResult(currentResult, calcDesc);
   createAndWriteOutput("+", initialResult, enteredNumber);
-  /*const logEntry = {
-    operation: "ADD",
-    prevResult: initialResult,
-    number: enteredNumber,
-    newResult: currentResult,
-  };
+  //const logEntry = {
+    //operation: "ADD",
+    //prevResult: initialResult,
+    //number: enteredNumber,
+    //newResult: currentResult,
+  //};
   //logEntries.push(enteredNumber);         //broj
-  logEntries.push(logEntry); //objekat
-  console.log(logEntry.operation);
-  console.log(logEntries);*/
+  //logEntries.push(logEntry); //objekat
+  //console.log(logEntry.operation);
+  //console.log(logEntries);
   logObject("ADD", initialResult, enteredNumber);
 }
 
@@ -62,6 +83,23 @@ function divide() {
   createAndWriteOutput("/", initialResult, enteredNumber);
   logObject("DIVIDE", initialResult, enteredNumber);
 }
+*/
+
+function add(){
+  calculateResult('ADD');
+}
+function subtract(){
+  calculateResult('SUBTRACT');
+}
+function multiply(){
+  calculateResult('MULTIPLY');
+}
+function divide(){
+  calculateResult('DIVIDE');
+}
+
+
+
 
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
